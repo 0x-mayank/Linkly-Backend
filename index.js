55cf6e6bbc9e3193902b16c95a3396907a4cecc0
0x-mayank <mayankm1946@gmail.com>
2025-08-17 00:04:01 +0530

    cors

--- index.js
+++ index.js
@@ -6,9 +6,18 @@ import dotenv from "dotenv";
 dotenv.config();
 
 
+
+
+
 const app =express();
 app.use(cors())
 app.use(express.json())
+
+app.use(cors({
+  origin: "*", 
+  methods: "GET,POST,PUT,DELETE",
+  credentials: true
+}));
 
 
 // database connection
